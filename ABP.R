9769a7d26a1b44d9d875a9867f91a0f6ae66a15c
andreagallardo6 <andrea.gallardo01@estudiant.upf.edu>
2021-01-23 21:00:14 +0100

    final

--- ABP.R
+++ ABP.R
@@ -1,21 +1,26 @@
 setwd("~/graficaABP")
 dat <- read.csv2("measles-notification-rate-12-months-December-2020.csv", sep = ",", stringsAsFactors = TRUE)
 
-dat1 <- data.frame(Pais = dat$X, "Casos per milio" = dat$X.2)
+dat1 <- data.frame(Pais = dat$X, "Casos per milió" = dat$X.2)
 dat2 <- dat1[-c(1,31,32,33),]
 
-dat4 <- data.frame(Pais = dat2$Pais, "Casos per milio" = as.numeric(as.character(dat2$Casos.per.milio)))
+dat3 <- as.numeric(as.character(dat2$Casos.per.milió))
 
-dat5 <- dat4[rev(order(dat4$Casos.per.milio)),]
+dat4 <- data.frame(Pais = dat2$Pais, "Casos per milió" = as.numeric(as.character(dat2$Casos.per.milió)))
+
+dat5 <- dat4[rev(order(dat4$Casos.per.milió)),]
+
+par(mar=c(6,3,3,1))
 
 g <- barplot(t(as.matrix(as.numeric(dat5[, 2]))),
-        beside = TRUE,
-        names.arg = dat2$Pais,
-        density = NULL,
-        col = c("lightblue","purple"),
-        ylim = c(0,60),
-        main = "Casos de xarampio a Europa 2020",
-        las = 2)
+             beside = TRUE,
+             names.arg = dat2$País,
+             density = NULL,
+             col = c("lightblue","purple"),
+             ylim = c(0,60),
+             main = "Casos de xarampió a Europa 2020",
+             las = 2,
+             cex.axis = 0.8,
+             cex.names = 0.8)
 
-text(g, dat5$Casos.per.milio, paste(dat5$Casos.per.milio, sep="") ,cex=0.5)
-
+text(g, dat5$Casos.per.milió+1.5, paste(round(dat5$Casos.per.milió, digits = 0)) ,cex=0.7)
