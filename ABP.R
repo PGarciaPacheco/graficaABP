5670f4e707925e160af77d94d653d5df7cb398bb
Paula Garcia <paula.garcia20@estudiant.upf.edu>
2021-01-23 20:04:48 +0100

    edit2

--- ABP.R
+++ ABP.R
@@ -1,13 +1,6 @@
 setwd("~/graficaABP")
 dat <- read.csv("measles-notification-rate-12-months-December-2020.csv", sep = ",", stringsAsFactors = TRUE)
-dat
+dat1 <- data.frame(Pais = dat$X, "Casos" = dat$X.2)
+dat2 <- dat1[-c(1,31,32,33),]
 
-dat1 <- data.frame(Pais = dat$X, "Casos per milió" = dat$X.2)
-dat2 <- dat1[-c(1),]
-dat2
-
-dat3 <- sort(dat2$Casos.per.milió, decreasing = TRUE)
-dat3
-
-horiz = TRUE
 
