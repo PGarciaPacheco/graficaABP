b7cb139027bf6e562ab1e41e7299204b0368476f
Paula Garcia <paula.garcia20@estudiant.upf.edu>
2021-01-23 20:58:49 +0100

    edit 4

--- ABP.R
+++ ABP.R
@@ -1,21 +1,21 @@
 setwd("~/graficaABP")
 dat <- read.csv2("measles-notification-rate-12-months-December-2020.csv", sep = ",", stringsAsFactors = TRUE)
 
-dat1 <- data.frame(País = dat$X, "Casos per milió" = dat$X.2)
+dat1 <- data.frame(Pais = dat$X, "Casos per milio" = dat$X.2)
 dat2 <- dat1[-c(1,31,32,33),]
 
-dat4 <- data.frame(Pais = dat2$Pais, "Casos per milió" = as.numeric(as.character(dat2$Casos.per.milió)))
+dat4 <- data.frame(Pais = dat2$Pais, "Casos per milio" = as.numeric(as.character(dat2$Casos.per.milio)))
 
-dat5 <- dat4[rev(order(dat4$Casos.per.milió)),]
+dat5 <- dat4[rev(order(dat4$Casos.per.milio)),]
 
 g <- barplot(t(as.matrix(as.numeric(dat5[, 2]))),
         beside = TRUE,
-        names.arg = dat2$País,
+        names.arg = dat2$Pais,
         density = NULL,
         col = c("lightblue","purple"),
         ylim = c(0,60),
-        main = "Casos de xarampió a Europa 2020",
-        las = 2,
-        horiz = TRUE)
+        main = "Casos de xarampio a Europa 2020",
+        las = 2)
 
-text(g, dat5$Casos.per.milió, paste(dat5$Casos.per.milió, sep="") ,cex=0.5)
+text(g, dat5$Casos.per.milio, paste(dat5$Casos.per.milio, sep="") ,cex=0.5)
+
