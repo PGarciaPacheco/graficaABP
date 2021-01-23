973cff92f8a957ab3e91746a9572d828aee22b7c
andreagallardo6 <andrea.gallardo01@estudiant.upf.edu>
2021-01-23 20:09:37 +0100

    editat3

--- ABP.R
+++ ABP.R
@@ -1,6 +1,23 @@
 setwd("~/graficaABP")
 dat <- read.csv("measles-notification-rate-12-months-December-2020.csv", sep = ",", stringsAsFactors = TRUE)
-dat1 <- data.frame(Pais = dat$X, "Casos" = dat$X.2)
+dat1 <- data.frame(País = dat$X, "Casos per milió" = dat$X.2)
 dat2 <- dat1[-c(1,31,32,33),]
 
+
+dat3 <- as.numeric(as.character(dat2$Casos.per.milió))
+
+dat4 <- data.frame(Pais = dat2$Pais, "Casos per milió" = as.numeric(as.character(dat2$Casos.per.milió)))
+
+dat5 <- dat4[rev(order(dat4$Casos.per.milió)),]
+dat5
+
+barplot(t(as.matrix(as.numeric(dat5[, 2]))),
+        beside = TRUE,
+        names.arg = dat2$País,,
+        density = NULL,
+        col = c("lightblue","purple","lightgreen"),
+        ylim = c(0,60),
+        main = "Casos de xarampió a Europa 2020",
+        las = 2,
+        horiz = TRUE)
 
